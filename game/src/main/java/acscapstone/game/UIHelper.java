a2ab3ba9120a2f556f1f63904f94098e05b59726
Joseph <joseph.e.oxford@gmail.com>
2026-03-27 10:44:30 -0700

    Made the button change color on "highlight". OMFG LETSGO I've also messed around a bunch with groovy styling, and I'm getting a hang of it I just need to research how to stop it from turning a lovely piss yellow when I click it

--- game/src/main/java/acscapstone/game/UIHelper.java
+++ game/src/main/java/acscapstone/game/UIHelper.java
@@ -13,7 +13,8 @@ public class UIHelper {
     public UIHelper() {}
 
     public Button makeButton(String text, AssetManager assetManager, AppSettings settings) {
-        Button button = new Button("Delete", new ElementId("danger.button"), "my_custom_style");
+        //Button button = new Button(text, new ElementId("button"), "solid_button");
+        Button button = new Button(text, new ElementId("red.button"), "style_1");
         button.setLocalTranslation(settings.getWidth() / 2f, settings.getHeight() / 2f, 0f);
         return button;
     }
