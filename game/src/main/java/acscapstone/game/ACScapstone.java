f8410a77db2976c9c48df088ce72184786d23f69
Joseph <joseph.e.oxford@gmail.com>
2026-04-07 08:21:36 -0700

    Moved generating spheres into the TestingHelper class. I'l figure out the scope issue later

--- game/src/main/java/acscapstone/game/ACScapstone.java
+++ game/src/main/java/acscapstone/game/ACScapstone.java
@@ -49,14 +49,8 @@ public class ACScapstone extends SimpleApplication {
         //rootNode.attachChild(geom1); // Actually add the geometry object (3D shape)
 
         attachUI(); // Attach all the UI stuffs
-        Vector3f[] x = new TestingHelper().values;
-        for (Vector3f v : x) {
-            Geometry geom2 = new Geometry("Sphere", new Sphere(35, 35, 1f));
-            geom2.rotateUpTo(new Vector3f(0f, 0f, -1f));
-            geom2.setMaterial(mat);
-            geom2.setLocalTranslation(v);
-            rootNode.attachChild(geom2);
-        }
+        TestingHelper x = new TestingHelper();
+        x.loadSpheres(rootNode, assetManager);
     }
 
     private void initKeys() {
--- game/src/main/java/acscapstone/game/TestingHelper.java
+++ game/src/main/java/acscapstone/game/TestingHelper.java
@@ -1,8 +1,13 @@
 package acscapstone.game;
 
 
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
 import com.jme3.math.Vector3f;
-
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Sphere;
+import com.jme3.texture.Texture;
 import java.util.Arrays;
 
 public class TestingHelper {
@@ -16,5 +21,19 @@ public class TestingHelper {
         System.out.println(Arrays.toString(values));
     }
 
+    public void loadSpheres(Node node, AssetManager assetManager) {
+
+        Texture tex = assetManager.loadTexture("Textures/man.jpg");
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+
+        for (Vector3f v : values) {
+            Geometry geom2 = new Geometry("Sphere", new Sphere(35, 35, 1f));
+            geom2.rotateUpTo(new Vector3f(0f, 0f, -1f));
+            geom2.setMaterial(mat);
+            geom2.setLocalTranslation(v);
+
+            node.attachChild(geom2);
+        }
+    }
 
 }
