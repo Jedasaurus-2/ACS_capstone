ac8af3d651dbadeab8ce31aaafafc5158b59a923
Joseph <joseph.e.oxford@gmail.com>
2026-04-17 09:40:18 -0700

    I added the labels! Now I need listeners...

--- game/src/main/java/acscapstone/game/LimitsUI.java
+++ game/src/main/java/acscapstone/game/LimitsUI.java
@@ -17,21 +17,30 @@ public class LimitsUI {
 
     public LimitsUI() {}
 
+    // Makes all of the UI
     public void attachUI(Node guiNode, Camera cam) {
         final String[] textForButtons = {"Apply X?","Apply Y?","Apply Z?"};
         final String[] textForLabels = {"Upper: ","Lower: "};
         BaseStyles.loadGlassStyle();
         float windowWidth = cam.getWidth();
         float windowHeight = cam.getHeight();
+        // Because of x, y, and z limits
         for (int x = 0; x < 3; x++) {
+
+            // Make it with the default glass style
             upperLimits[x] = new Slider("glass");
+            // Set the values the slider has
             upperLimits[x].getModel().setMinimum(0);
             upperLimits[x].getModel().setMaximum(100);
+            // Set how the slider it
             upperLimits[x].setPreferredSize(new Vector3f(250f, 25f, 0f));
+            // Set the grabby thing's size
             upperLimits[x].getThumbButton().setPreferredSize(new Vector3f(25f, 25f, 0));
+            // Default position, won't ever be seen
             upperLimits[x].setLocalTranslation(windowWidth * 0.5f, windowHeight * 0.5f, 0);
             guiNode.attachChild(upperLimits[x]);
 
+            // Same thing again
             lowerlimits[x] = new Slider("glass");
             lowerlimits[x].getModel().setMinimum(0);
             lowerlimits[x].getModel().setMaximum(100);
@@ -40,11 +49,14 @@ public class LimitsUI {
             lowerlimits[x].setLocalTranslation(windowWidth * 0.5f, windowHeight * 0.5f, 0);
             guiNode.attachChild(lowerlimits[x]);
 
+            // Checkmark boxes
             applyButtons[x] = new Checkbox(textForButtons[x], "glass");
+            // Won't be seen
             applyButtons[x].setLocalTranslation(windowWidth * 0.5f, windowHeight * 0.5f, 0);
             guiNode.attachChild(applyButtons[x]);
 
             if (x < 2) {
+                // Makes the labels
                 xSliderLabels[x] = new Label(textForLabels[x], "glass");
                 guiNode.attachChild(xSliderLabels[x]);
                 ySliderLabels[x] = new Label(textForLabels[x], "glass");
@@ -53,15 +65,20 @@ public class LimitsUI {
                 guiNode.attachChild(zSliderLabels[x]);
             }
         }
+        
     }
 
+    // Called when the window resizes
+    // Sets the position of everything based on screen width and height (w, h)
     public void reshape(int w, int h) {
         for (int x = 0; x < 3; x++) {
+            // Bottom left corner. Offset by x and 25 (about the height of a slider)
             if (upperLimits[x] != null)
                 upperLimits[x].setLocalTranslation(
                         0f,
                         h * (x + 1) * 0.1f - upperLimits[x].getPreferredSize().y * 0.5f + 25,
                         0f);
+            // Also bottom left corner. Offset by x
             if (lowerlimits[x] != null)
                 lowerlimits[x].setLocalTranslation(
                         0f,
@@ -72,14 +89,17 @@ public class LimitsUI {
                         0f,
                         h * (x + 1) * 0.1f +  applyButtons[x].getPreferredSize().y * 0.5f + 25,
                         0f);
-            if (x == 1 && upperLimits[x] != null) {
-                if (xSliderLabels[x] != null)
-                    xSliderLabels[x].setLocalTranslation(
-                            upperLimits[x].getPreferredSize().x,
-                        h * (x + 1) * 0.1f - upperLimits[x].getPreferredSize().y * 0.5f + 25,
-                        0);
-                if (ySliderLabels[x] != null) ySliderLabels[x].setLocalTranslation(upperLimits[x].getPreferredSize().x, h * 0.5f, 0);
-                if (zSliderLabels[x] != null) zSliderLabels[x].setLocalTranslation(upperLimits[x].getPreferredSize().x, h * 0.5f, 0);
+            // Upper labels
+            if (x == 0 && upperLimits[x] != null && xSliderLabels[x] != null && ySliderLabels[x] != null && zSliderLabels[x] != null) {
+                xSliderLabels[x].setLocalTranslation(upperLimits[x].getPreferredSize().x, h * 0.1f - upperLimits[x].getPreferredSize().y * 0.5f + 25, 0);
+                ySliderLabels[x].setLocalTranslation(upperLimits[x].getPreferredSize().x, h * 0.2f - upperLimits[x].getPreferredSize().y * 0.5f + 25, 0);
+                zSliderLabels[x].setLocalTranslation(upperLimits[x].getPreferredSize().x, h * 0.3f - upperLimits[x].getPreferredSize().y * 0.5f + 25, 0);
+            }
+            // Lower labels
+            if (x == 1 && upperLimits[x] != null && xSliderLabels[x] != null && ySliderLabels[x] != null && zSliderLabels[x] != null) {
+                xSliderLabels[x].setLocalTranslation(upperLimits[x].getPreferredSize().x, h * 0.1f - upperLimits[x].getPreferredSize().y * 0.5f, 0);
+                ySliderLabels[x].setLocalTranslation(upperLimits[x].getPreferredSize().x, h * 0.2f - upperLimits[x].getPreferredSize().y * 0.5f, 0);
+                zSliderLabels[x].setLocalTranslation(upperLimits[x].getPreferredSize().x, h * 0.3f - upperLimits[x].getPreferredSize().y * 0.5f, 0);
             }
         }
     }
